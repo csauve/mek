69d70435549362c99a099af616ba7dec2c4e82e9
csauve <csauve@t3hz0r.com>
2021-01-09 01:11:23 -0800

    Add color information to WRL to OBJ conversion tool

--- tools_misc/wrl_to_obj.py
+++ tools_misc/wrl_to_obj.py
@@ -2,10 +2,11 @@
 import io
 import sys
 import tatsu
+import re
 from argparse import ArgumentParser
 
 # Author: Conscars <inbox@t3hz0r.com>, 2020-03-05
-VERSION_STR = "1.0.0"
+VERSION_STR = "1.1.0"
 
 # What follows is an EBNF grammar for the subset of VRML 1.0 which tool ouputs.
 # It's used to generate a parser which can load the files into syntax tree for
@@ -97,6 +98,74 @@ def parse_wrl_to_ast(wrl_content):
     '''
     return WRL_PARSER.parse(wrl_content)
 
+def infer_error_type(binding_type, mtl_diffuse_colors):
+    '''
+    Infer the type of error based on color used by Tool
+    '''
+    # thanks to dt192 for this trick!
+    color_names = {
+        "1.000000,0.000000,0.000000": "red",
+        "0.000000,1.000000,0.000000": "green",
+        "1.000000,0.500000,0.000000": "orange",
+        "0.000000,1.000000,1.000000": "cyan",
+        "1.000000,1.000000,0.000000": "yellow",
+        "1.000000,0.000000,1.000000": "magenta",
+        "0.000000,0.000000,0.000000": "black",
+        "0.000000,0.000000,1.000000": "blue",
+        # unconfirmed values:
+        }
+
+    if mtl_diffuse_colors:
+        found_colors = set()
+        for color in mtl_diffuse_colors:
+            color_str = ",".join([color.x, color.y, color.z])
+            color_name = color_names.get(color_str, color_str)
+            found_colors.add(color_name)
+        color_info = " (" + ", ".join(sorted(found_colors)) + ")"
+
+        if binding_type == "PER_FACE":
+            ### WARNING found nearly coplanar surfaces (red and green).
+            if "red" in found_colors and "green" in found_colors:
+                return "nearly coplanar surfaces" + color_info
+            ### WARNING found #1 degenerate triangles.
+            ### ERROR found z buffered triangles (red).
+            if "red" in found_colors:
+                return "degenerate or z-buffered triangle" + color_info
+            ### WARNING: portal outside the bsp. [see magenta in error geometry]
+            if "magenta" in found_colors:
+                return "portal outside BSP" + color_info
+        elif binding_type == "PER_VERTEX":
+            ### ERROR edge #%d is open (red)
+            ### ERROR couldn't update edge #%d (red)
+            ### ERROR edge #%d is too short (red)
+            # edge has more than four triangles (see red in error geometry)
+            if "red" in found_colors:
+                return "bad edge" + color_info
+            ### WARNING unearthed edge (magenta boxed lines)
+            ### WARNING found possible T-junction (pink).
+            if "magenta" in found_colors:
+                return "unearthed edge or T-junction" + color_info
+        ### WARNING: a surface clipped to no leaves (see cyan in error geometry)
+        if "cyan" in found_colors:
+            return "surface clipped to no leaves" + color_info
+        ### WARNING: portal doesn't divide any space (it may be coincident with seam sealer?). [see green in error geometry]
+        if "green" in found_colors:
+            return "portal does not divide space" + color_info
+        ### ERROR: portal does not define two closed spaces. (see yellow in error geometry)
+        if "yellow" in found_colors:
+            return "portal does not define two closed spaces" + color_info
+        ### WARNING: found duplicate triangle building connected geometry. YOU SHOULD FIX THIS. (see orange in error geometry)
+        ### ERROR couldn't build bsp because of overlapping surfaces (orange)
+        if "orange" in found_colors:
+            return "duplicate triangle or overlapping surface" + color_info
+        #two fog planes intersected in a cluster (see black in error geometry).
+        if "black" in found_colors:
+            return "two fog planes intersected in a cluster" + color_info
+        #degenerate triangle [or triangle with bad uvs] (see blue in error geometry)
+        if "blue" in found_colors:
+            return "degenerate triangle or UVs" + color_info
+    return "unknown" + color_info
+
 def convert_wrl_to_obj(input_stream, output_stream):
     '''
     Translates the WRL input stream to an OBJ output stream.
@@ -106,33 +175,30 @@ def convert_wrl_to_obj(input_stream, output_stream):
         print(line, file=output_stream)
 
     vert_index = 1 # Vertex indexes start at 1 in OBJ
-    lines = []
-    faces = []
+    object_data = []
 
     output("# This is a converted tool.exe WRL file. Import with settings Y forward and Z up")
 
     for separator in parse_wrl_to_ast(input_stream.read()):
+        object_data.append("o Error geometry: " + infer_error_type(separator.mtl_binding, separator.mtl_diffuse_colors))
         # For lines and faces, convert the separator-relative indexes into
         # global OBJ-relative ones, which we add to arrays to print later:
         if separator.indexed_faces:
             for indexed_face in separator.indexed_faces:
-                faces.append([int(i) + vert_index for i in indexed_face])
+                vert_indices = [int(i) + vert_index for i in indexed_face]
+                object_data.append("f {0} {1} {2}".format(*vert_indices))
         if separator.indexed_lines:
             for indexed_line in separator.indexed_lines:
-                lines.append([int(i) + vert_index for i in indexed_line])
+                vert_indices = [int(i) + vert_index for i in indexed_line]
+                object_data.append("l {0} {1}".format(*vert_indices))
         for coord in separator.coords:
             # Put a list of all vertices at the start of the output
             output("v {x} {y} {z}".format(**coord))
             vert_index += 1
 
-    # Finish up by outputting all the faces and lines. Note that all the
-    # grouping information was lost in this process and we are ignoring some
-    # data from the WRL. There's room for improvement here to infer error type
-    # by colour, and name objects. Would be best as a Blender plugin.
-    for face in faces:
-        output("f {0} {1} {2}".format(*face))
-    for line in lines:
-        output("l {0} {1}".format(*line))
+    # Finish up by outputting all the objects (faces and line segments)
+    for line in object_data:
+        output(line)
 
 if __name__== "__main__":
     parser = ArgumentParser(description="Converts tool's .wrl files from VRML1.0 format to OBJ for importing to Blender. Version %s" % VERSION_STR)
